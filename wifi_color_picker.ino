11e5e0774803a25b57ef531689ab42291230b5b8
yuri-rage <yuri.rage@gmail.com>
2015-01-16 15:36:46 -0600

    minor doc changes

--- wifi_color_picker.ino
+++ wifi_color_picker.ino
@@ -1,15 +1,21 @@
-/* 
+/*
+ *****
+ from https://github.com/mat-lo/yun-examples/blob/master/color_picker/color_picker.ino
  HTML page with a custom color picker that controls an RGB led.
  The html page is located into the "www" folder of this sketch. 
  Open the sketch folder from the top menu: Sketch->Show Sketch Folder
 
- You can then go to http://arduino.local/sd/color_picker
+ You can then go to http://arduino.local/sd/wifi_color_picker
  to see the output of this sketch, where "arduino" is your 
  board name. 	
 
  09/2013 - Matteo Loglio (matlo.me)
-
- yuri - modified Jan 2015 	   
+ *****
+ 
+ yuri - modified Jan 2015
+   I used Matteo's code as a starting point to get familiar with Yun's bridge
+   The code has changed very significantly, and the HTML page is entirely different.
+   Matteo's web UI did not work well at all with mobile device browsers.
 */
 
 // uncomment to invert RGB values (common anode configuration)
